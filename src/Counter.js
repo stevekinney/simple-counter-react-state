e85ef3aaed210a96f40ebab20a24c3dc7bb5717f
Steve Kinney <hello@stevekinney.net>
2019-11-21 10:32:05 -0600

    Finish up local storage

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,62 @@
 import React, { Component } from 'react';
 
+const getStateFromLocalStorage = () => {
+  const storage = localStorage.getItem('counterState');
+  console.log(storage);
+  if (storage) return JSON.parse(storage);
+  return { count: 0 };
+};
+
+const storeStateInLocalStorage = state => {
+  localStorage.setItem('counterState', JSON.stringify(state));
+  console.log(localStorage);
+};
+
+document.title = 'Hello';
+
 class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = getStateFromLocalStorage();
+
+    console.log(this.state);
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  increment() {
+    this.setState(
+      (state, props) => {
+        const { max, step } = props;
+        if (state.count >= max) return;
+        return { count: state.count + step };
+      },
+      () => storeStateInLocalStorage(this.state),
+    );
+
+    console.log('Before!', this.state);
+  }
+
+  decrement() {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  reset() {
+    this.setState({ count: 0 });
+  }
+
   render() {
+    const { count } = this.state;
+
     return (
       <div className="Counter">
-        <p className="count">0</p>
+        <p className="count">{count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+          <button onClick={this.reset}>Reset</button>
         </section>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const Application = () => {
   return (
     <main className="Application">
       <section className="Counters">
-        <Counter />
+        <Counter max={15} step={5} />
       </section>
     </main>
   );
