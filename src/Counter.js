242a81cee8bc306161134f2dcebbc0d8074336ac
Steve Kinney <hello@stevekinney.net>
2019-11-20 18:41:13 -0600

    Update Counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  increment = () => {
+    this.setState(({ count }) => ({
+      count: count + 1,
+    }));
+  };
+
+  decrement = () => {
+    this.setState(({ count }) => ({
+      count: count - 1,
+    }));
+  };
+
+  reset = () => {
+    this.setState(() => ({
+      count: 0,
+    }));
+  };
+
   render() {
+    const { count } = this.state;
+
     return (
       <div className="Counter">
-        <p className="count">0</p>
+        <p className="count">{count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+          <button onClick={this.reset}>Reset</button>
         </section>
       </div>
     );
